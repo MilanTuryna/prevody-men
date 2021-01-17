e97b44acf347f1a536cf202b5657bdd73f49c6e1
MilanTuryna <miloslavturyna@seznam.cz>
2021-01-17 16:53:47 +0100

    Updated config.REQUEST.classic because yacdn.org isn't work in js\config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 export const config = {
     REQUEST: {
-        classic: `https://yacdn.org/serve/https://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.xml`,
+        classic: `https://cors-anywhere.herokuapp.com/https://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.xml`,
         backup: `https://cors-anywhere.herokuapp.com/https://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.xml`,
         with_date: `https://yacdn.org/proxy/https://www.cnb.cz/cs/financni_trhy/devizovy_trh/kurzy_devizoveho_trhu/denni_kurz.xml?date=`,
         original: {
